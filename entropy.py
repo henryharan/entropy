6e7a4a1517014e197f1e109c11ead9edd53d60dd
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-04 00:22:37 +0300

    Update entropy.py

--- entropy.py
+++ entropy.py
@@ -66,7 +66,7 @@ group2.add_argument("-l","--list",dest='inputfile',help="The camera's IP:port ad
 group2.add_argument("--shodan",dest='shodan',help="Your Shodan API Key. You can get help from https://www.shodan.io/",type=str)
 group2.add_argument("--zoomeye",dest='zoomeye',help="Your ZoomEye API Key. You can get help from https://www.zoomeye.org/api",type=str)
 parser.add_argument("-u","--update",dest='update',help="Update Entropy Exploit.",action='store_true')
-parser.add_argument("--info",dest='info',help="Show Entropy Exploit details.",action='store_true')
+parser.add_argument("--details",dest='info',help="Show Entropy Exploit details.",action='store_true')
 
 args=parser.parse_args()
 
@@ -74,7 +74,7 @@ if args.update:
     os.system("chmod +x bin/entropy && bin/entropy -u")
     sys.exit()
     
-if args.info:
+if args.details:
     os.system("cat banner/banner.txt")
     print("")
     os.system("cat banner/banner1.txt")
