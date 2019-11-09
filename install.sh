a3e340916b50e0f7b7cafdcac5ee0c4205a86945
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-10 00:26:33 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,14 +28,14 @@ C="\033[0m"
 CE="\033[0m"
 RS="\033[31m"
 YS="\033[1;33m"
-BS="-e \033[34m"
+BS="-e \033[37m[*] "
 
 R="\033[31m"
 
 if [[ $EUID -ne 0 ]]
 then
    sleep 1
-   echo -e ""$RS"This script must be run as root!"$CE"" 1>&2
+   echo -e ""$RS"[!] This script must be run as root!"$CE"" 1>&2
    sleep 1
    exit
 fi
